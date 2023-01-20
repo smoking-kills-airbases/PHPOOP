3db751cc087eafb6dd3f019e8d85287f0bdb9445
smoking-kills-airbases <gelzbetonis369@gmail.com>
2023-01-20 10:34:48 +0200

    Update NewCustomerPriceCalculator.php

--- ANTRA/Classes/NewCustomerPriceCalculator.php
+++ ANTRA/Classes/NewCustomerPriceCalculator.php
@@ -2,7 +2,22 @@
 
 namespace Classes;
 
-class NewCustomerPriceCalculator
-{
+use Interfaces\TotalCalculatorInterface;
 
+class NewCustomerPriceCalculator implements TotalCalculatorInterface
+{
+    public function calculatePrice($items): float
+    {
+        $totalPrice = 0;
+        $first = true;
+        foreach ($items as $item) {
+            if ($first) {
+                $totalPrice += ($item->price - ($this->price * 0.2));
+                $first = false;
+            } else {
+                $totalPrice += $item->price;
+            }
+        }
+        return $totalPrice;
+    }
 }
